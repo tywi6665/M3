16c4ad0b53c122d85acf91b8135d740334509d89
tywi6665 <tyletcole@gmail.com>
2019-09-30 21:01:42 -0600

    consolidated banner component with container

--- src/Components/Container.js
+++ src/Components/Container.js
@@ -1,8 +1,12 @@
 import React from "react";
 
-const Container = ({ number, children }) => {
+const Container = ({ number, header, subHeader, children }) => {
     return (
         <div className={`container section-${number}`}>
+            <div className="banner">
+                <p className="header">{header}</p>
+                <p className="sub-header">{subHeader}</p>
+            </div>
             {children}
         </div>
     );
