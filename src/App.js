57bfe594e0fedfce80dc3730cb0adb38ae0f2eb3
tywi6665 <tyletcole@gmail.com>
2019-10-02 15:48:29 -0600

    styling the banner

--- src/App.js
+++ src/App.js
@@ -11,34 +11,34 @@ function App() {
         <Navbar />
         <Container
           number="1"
-          header="The Main Section"
+          header={["The Main", "Section"]}
           subHeader="The Main Section"
         >
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </Container>
         <Container
           number="2"
-          header="The Chart Section"
+          header={["The Chart", "Display"]}
           subHeader="The Chart Section"
         >
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </Container>
         <Container
           number="3"
-          header="The Other Section"
+          header={["The Other", "Section"]}
           subHeader="The Other Section"
         >        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </Container>
         <Container
           number="4"
-          header="The OtherOther Section"
+          header={["The OtherOther", "Section"]}
           subHeader="The OtherOther Section"
         >
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </Container>
         <Container
           number="5"
-          header="The OtherOtherOther Section"
+          header={["The OtherOtherOther", "Section"]}
           subHeader="The OtherOtherOther Section"
         >
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
--- src/Components/Container.js
+++ src/Components/Container.js
@@ -4,7 +4,7 @@ const Container = ({ number, header, subHeader, children }) => {
     return (
         <div className={`container section-${number}`}>
             <div className="banner">
-                <p className="header">{header}</p>
+                <p className="header">{header[0]}<span> {header[1]}</span></p>
                 <p className="sub-header">{subHeader}</p>
             </div>
             {children}
