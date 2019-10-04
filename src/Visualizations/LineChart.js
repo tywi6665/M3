ba9001f9662e91ff62d4d206f51a8a9fd13ec0b0
tywi6665 <tyletcole@gmail.com>
2019-10-04 16:16:47 -0600

    imported and parsed through csv file

--- src/Visualizations/LineChart.js
+++ src/Visualizations/LineChart.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as d3 from "d3";
+import data from "./csv.csv";
 
 const height = 100,
-    width = 100
+    width = 100,
+    margin = { top: 20, right: 0, bottom: 20, left: 20 }
 
 const LineChart = () => {
+
+    const [spending, setSpending] = useState(null);
+
+    d3.csv(data).then(function (data) {
+        console.log(data)
+    }).catch(function (error) {
+        console.log(error)
+    })
+
     return (
         <svg height={`${height}%`} width={`${width}%`}></svg>
     );
