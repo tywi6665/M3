2727914b8f6d103b8ea326c305edb50a149d06e0
tywi6665 <tyletcole@gmail.com>
2019-10-04 15:50:27 -0600

    created LineChart component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import MasterContainer from "./Components/MasterContainer";
 import Container from "./Components/Container";
 import './App.scss';
 import Navbar from './Components/Navbar';
+import LineChart from './Visualizations/LineChart';
 
 function App() {
   return (
@@ -21,7 +22,7 @@ function App() {
           header={["The Chart", "Display"]}
           subHeader="The Chart Section"
         >
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <LineChart />
         </Container>
         <Container
           number="3"
---
+++ src/Visualizations/LineChart.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as d3 from "d3";
+
+const height = 100,
+    width = 100
+
+const LineChart = () => {
+    return (
+        <svg height={`${height}%`} width={`${width}%`}></svg>
+    );
+}
+
+export default LineChart;
