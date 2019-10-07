551b079b9c28bc5d81f185164035c8afc279fc61
tywi6665 <tyletcole@gmail.com>
2019-10-06 20:37:44 -0600

    added linearGradient tag to svg

--- src/Visualizations/LineChart.js
+++ src/Visualizations/LineChart.js
@@ -11,7 +11,16 @@ const LineChart = () => {
     const [spending, setSpending] = useContext(SpendingDataContext);
 
     return (
-        <svg height={`${height}%`} width={`${width}%`}></svg>
+        <svg height={`${height}%`} width={`${width}%`}>
+            <linearGradient id="spending-gradient" x1="0%" y1="0%" x2="100%" y2="0%">
+                <stop offset="0%" style="stop-color:rgba(255,191,141,1);stop-opacity:1" />
+                <stop offset="25%" style="stop-color:rgba(252,167,146,1);stop-opacity:1" />
+                <stop offset="50%" style="stop-color:rgba(206,116,198,1);stop-opacity:1" />
+                <stop offset="75%" style="stop-color:rgba(148,65,161,1);stop-opacity:1" />
+                <stop offset="100%" style="stop-color:rgba(118,48,201,1);stop-opacity:1" />
+            </linearGradient>
+            <path className="line" d={spending} fill="none" />
+        </svg>
     );
 }
 
