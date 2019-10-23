8baecf31e39893c0e181ea78c3d0766c8522005e
tywi6665 <tyletcole@gmail.com>
2019-10-22 20:47:42 -0600

    styling listitem

--- src/Components/ListItem.js
+++ src/Components/ListItem.js
@@ -4,7 +4,7 @@ const ListItem = ({ transactionName, transactionAmount }) => {
     return (
         <div className="list-item">
             <div className="item-name"><div></div>{transactionName}</div>
-            <div className="item-amount"><div></div>{transactionAmount}</div>
+            <div className="item-amount"><div className="down"></div>{transactionAmount}</div>
         </div>
     );
 }
