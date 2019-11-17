65a86fe95b3eca4dfab65fba4a8ad5eb301d08d8
tywi6665 <tyletcole@gmail.com>
2019-11-17 08:47:21 -0700

    started working on category display section

--- src/App.js
+++ src/App.js
@@ -76,8 +76,8 @@ function App() {
           </Container>
           <Container
             number="5"
-            header={["The OtherOtherOther", "Section"]}
-            subHeader="The OtherOtherOther Section"
+            header={["Overall Spending by Category", "Display"]}
+            subHeader="Your Transaction History Broken Down by Category"
           >
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           </Container>
