7e13ebccb105313e8c37624d943f7bbbbdcdb837
tywi6665 <tyletcole@gmail.com>
2019-11-24 08:04:38 -0700

    converted list-item divs into ul's and li's

--- src/Components/ListItem.js
+++ src/Components/ListItem.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const ListItem = ({ transactionName, transactionAmount, transactionCategory, categoryAmount, list }) => {
     return (
-        <div className="list-item">
+        <ul className="list-item">
             {list === "recent" ? [
-                (<>
+                (<li>
                     <div className="item-name">
                         <div className={transactionAmount < 0 ? "circle-down" : "circle-up"}></div>
                         {transactionName}
@@ -13,15 +13,15 @@ const ListItem = ({ transactionName, transactionAmount, transactionCategory, cat
                         <div className={transactionAmount < 0 ? "arrow-down" : "arrow-up"}></div>
                         {transactionAmount < 0 ? transactionAmount.replace("-", "-$") : `$${transactionAmount}`}
                     </div>
-                </>)
+                </li>)
             ] : [
-                    (<>
+                    (<li>
                         <div className="item-name">{transactionCategory.toUpperCase()}</div>
                         <div className="item-amount">{`$${Math.abs(categoryAmount)}`}</div>
-                    </>)
+                    </li>)
                 ]}
 
-        </div>
+        </ul>
     );
 }
 
