aefa2dd351498a77b971c9d72d850a8bf6463bde
tywi6665 <tyletcole@gmail.com>
2019-12-02 20:32:49 -0700

    starting to build out svg

--- src/Visualizations/CategoryChart.js
+++ src/Visualizations/CategoryChart.js
@@ -7,19 +7,30 @@ const CategoryChart = ({ width }) => {
     const [rawSpending, setRawSpending] = useContext(SpendingDataContext);
     const [categories, setCategories] = useState(null);
 
+    const svgHeight = 300,
+        svgWidth = width,
+        innerRadius = 100,
+        outerRadius = 300,
+        margin = { top: 20, right: 15, bottom: 20, left: 15 };
+
     useEffect(() => {
         if (categories === null) {
             return;
         }
 
-    }, [width])
+        const packLayout = d3.pack()
+            .size([outerRadius, outerRadius]);
 
-    const svgHeight = 300,
-        svgWidth = width,
-        margin = { top: 20, right: 15, bottom: 20, left: 15 };
+        // const rootNodes = d3.hierarchy(categories);
+
+    }, [width]);
 
     return (
-        <svg height={svgHeight} width={window.innerWidth > 1000 ? 422 : svgWidth}></svg>
+        <svg height={svgHeight} width={window.innerWidth > 1000 ? 422 : svgWidth}>
+            <g>
+                {/* Once data is parsed, map through array to output correct number of category circles */}
+            </g>
+        </svg>
     );
 }
 
